d691322ed8301caa82c19380bd5d96ed1e903445
Nathan Faucett <nathanfaucett@gmail.com>
2022-09-24 11:46:12 -0400

    prepare for publish

--- esm/index.js
+++
@@ -1,5 +0,0 @@
-export { Edge } from "./Edge";
-export { Graph, getParentPathAndKey } from "./Graph";
-export { Node } from "./Node";
-export {} from "./Ref";
-export { Ref } from "./Ref";
--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-export type { IEdgeJSON } from "./Edge";
-export { Edge } from "./Edge";
-export type { IGraphEvents, IDeleteJSON } from "./Graph";
-export { Graph, getParentPathAndKey } from "./Graph";
-export type { INodeJSON } from "./Node";
-export { Node } from "./Node";
-export { IRefJSON } from "./Ref";
-export { Ref } from "./Ref";
-export type { IKeyOf, IValueOf, IPrimitive, IGraph, IGraphValue, IRefValue, IRefValueChild, } from "./types";
